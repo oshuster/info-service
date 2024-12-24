341a8be0b4613bd948c8b69776b1bf84cbc8d5a1
oshuster <shuster.oleksandr@gmail.com>
2024-12-24 21:07:44 +0200

    [*] update swagerdoc (professions)

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -22,58 +22,60 @@ const options = {
     ],
     components: {
       schemas: {
-        // Схема для пошуку професій
-        ProfessionSearchRequest: {
-          type: 'object',
-          properties: {
-            query: {
-              type: 'string',
-              description: 'Пошуковий запит по назві або коду професії',
-              example: 'інженер',
+        professions: {
+          // Схема для пошуку професій
+          ProfessionSearchRequest: {
+            type: 'object',
+            properties: {
+              query: {
+                type: 'string',
+                description: 'Пошуковий запит по назві або коду професії',
+                example: 'інженер',
+              },
+            },
+            required: ['query'],
+          },
+          Profession: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer',
+                description: 'Унікальний ідентифікатор професії',
+                example: 1,
+              },
+              code_kp: {
+                type: 'string',
+                description: 'Код класифікації професії',
+                example: '1234',
+              },
+              name: {
+                type: 'string',
+                description: 'Назва професії',
+                example: 'Інженер',
+              },
             },
           },
-          required: ['query'],
-        },
-        Profession: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer',
-              description: 'Унікальний ідентифікатор професії',
-              example: 1,
+          EditProfession: {
+            type: 'object',
+            properties: {
+              id: {
+                type: 'integer',
+                description: 'Унікальний ідентифікатор професії',
+                example: 1,
+              },
+              code_kp: {
+                type: 'string',
+                description: 'Код класифікації професії',
+                example: '1234',
+              },
+              name: {
+                type: 'string',
+                description: 'Назва професії',
+                example: 'Інженер',
+              },
             },
-            code_kp: {
-              type: 'string',
-              description: 'Код класифікації професії',
-              example: '1234',
-            },
-            name: {
-              type: 'string',
-              description: 'Назва професії',
-              example: 'Інженер',
-            },
+            required: ['id', 'code_kp', 'name'],
           },
-        },
-        EditProfession: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer',
-              description: 'Унікальний ідентифікатор професії',
-              example: 1,
-            },
-            code_kp: {
-              type: 'string',
-              description: 'Код класифікації професії',
-              example: '1234',
-            },
-            name: {
-              type: 'string',
-              description: 'Назва професії',
-              example: 'Інженер',
-            },
-          },
-          required: ['id', 'code_kp', 'name'],
         },
       },
       responses: {
